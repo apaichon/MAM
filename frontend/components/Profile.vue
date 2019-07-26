422e7f75e5ccaf145457c7ee2bdd0d4a6995afe9
nicscthongchai <nicscthongchai@gmail.com>
2019-07-26 15:38:40 +0700

    Add profile page

--- frontend/components/Profile.vue
+++ frontend/components/Profile.vue
@@ -1,29 +1,56 @@
 <template>
-  <div class="user-profile-container">
-    <img
-      id="profile-image"
-      src="https://image.flaticon.com/icons/svg/236/236832.svg"
-      alt="user-image"
-    />
-    <div>
-      <md-button>Default</md-button>
+  <div class="md-layout md-alignment-center-center profile-container">
+    <div class="md-layout-item md-size-100">
+      <img
+        src="https://image.flaticon.com/icons/svg/236/236832.svg"
+        alt="user-image"
+        width="200"
+        height="200"
+      />
+    </div>
+    <div class="md-layout-item md-size-33 font-2x"><i class="fas fa-coins"></i> 9999</div>
+    <div class="md-layout-item md-size-33 font-2x"><i class="fas fa-dollar-sign"></i> 9999</div>
+    <md-field>
+      <label>Email</label>
+      <md-input v-model="profile.email" disabled></md-input>
+      <span class="md-helper-text"></span>
+    </md-field>
+    <md-field>
+      <label>First Name</label>
+      <md-input v-model="profile.firstName"></md-input>
+      <span class="md-helper-text"></span>
+    </md-field>
+    <md-field>
+      <label>Last Name</label>
+      <md-input v-model="profile.lastName"></md-input>
+      <span class="md-helper-text"></span>
+    </md-field>
+    <md-field>
+      <label>Mobile No.</label>
+      <md-input v-model="profile.mobileNo"></md-input>
+      <span class="md-helper-text"></span>
+    </md-field>
+    <div class="md-layout-item md-size-100">
+      <md-button class="md-raised md-primary">Update</md-button>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-
-}
+  props: ["profile"],
+  mounted() {}
+};
 </script>
 
 
 <style scoped>
-.user-profile-container {
+.profile-container {
   text-align: center;
+  padding: 5px 15px;
 }
 
-#profile-image {
-  max-width: 200px;
+.font-2x {
+  font-size: 200%;
 }
 </style>
--- frontend/pages/profile.vue
+++ frontend/pages/profile.vue
@@ -1,16 +1,49 @@
 <template>
   <div class="container">
-    <Profile />
+    <Profile v-bind:profile="profile[0]" />
   </div>
 </template>
 
 <script>
-import Profile from '~/components/Profile.vue'
+import axios from "axios";
+import https from "https";
+import Profile from "~/components/Profile.vue";
 
 export default {
+  async asyncData({ params }) {
+    let response = await axios({
+      method: "post",
+      url: process.env.baseUrl,
+      headers: {
+        "Content-Type": "application/json",
+        application: "Thai Stringers",
+        objectfile: "../../biz/AccountProfileBiz",
+        objectname: "AccountProfileBiz",
+        objectmethod: "Find"
+      },
+      httpsAgent: new https.Agent({
+        rejectUnauthorized: false
+      })
+    });
+
+    return { profile: response.data.data };
+  },
   components: {
     Profile
+  },
+  methods: {
+    find() {}
   }
+  // async asyncData({ params }) {
+  //   let { data } = await axios({
+  //     method: "get",
+  //     url: "https://reqres.in/api/users/1"
+  //   });
+
+  //   return {
+  //     firstname: data.first_name
+  //   };
+  // }
 };
 </script>
 

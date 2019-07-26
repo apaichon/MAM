9dc95ef2554d38fcbf39ff84c5bd3afa090507d8
nicscthongchai <nicscthongchai@gmail.com>
2019-07-26 09:56:21 +0700

    Add profile page

---
+++ frontend/components/Profile.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="user-profile-container">
+    <img
+      id="profile-image"
+      src="https://image.flaticon.com/icons/svg/236/236832.svg"
+      alt="user-image"
+    />
+  </div>
+</template>
+
+<style scoped>
+.user-profile-container {
+  text-align: center;
+}
+
+#profile-image {
+  max-width: 200px;
+}
+</style>
---
+++ frontend/pages/profile.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <Profile />
+  </div>
+</template>
+
+<script>
+import Profile from '~/components/Profile.vue'
+
+export default {
+  components: {
+    Profile
+  }
+};
+</script>
+
+<style scoped>
+</style>

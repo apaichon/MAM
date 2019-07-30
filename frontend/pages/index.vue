2e39711084d0b2b9e8371a1eb709211880572465
nicscthongchai <nicscthongchai@gmail.com>
2019-07-30 16:57:07 +0700

    Update index page

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -2,10 +2,10 @@
   <div class="container">
     <div>
       <logo />
-      <h1 class="title">
+      <h1 class="title" style="line-height: 100px;">
         frontend
       </h1>
-      <h2 class="subtitle">
+      <h2 class="subtitle" style="line-height: 50px;">
         MAM Project
       </h2>
       <div class="links">

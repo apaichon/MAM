a4e38288c513a4585866bef67606879cae87e117
sirimainson <sirimainson@gmail.com>
2019-08-06 02:58:26 +0700

    add table component

--- frontend/plugins/global-component.js
+++ frontend/plugins/global-component.js
@@ -3,8 +3,11 @@ import ChartCard from '../components/Card/ChartCard'
 import StatsCard from '../components/Card/StatsCard'
 import NavTabsCard from '../components/Card/NavTabsCard'
 import NavTabsTable from '../components/Tables/NavTabsTable.vue'
+import OrderedTable from '../components/Tables/OrderedTable.vue'
+import SimpleTable from '../components/Tables/OrderedTable.vue'
 
 Vue.component('chart-card', ChartCard)
 Vue.component('stats-card', StatsCard)
 Vue.component('nav-tabs-card', NavTabsCard)
 Vue.component('nav-tabs-table', NavTabsTable)
+Vue.component('simple-table', SimpleTable)

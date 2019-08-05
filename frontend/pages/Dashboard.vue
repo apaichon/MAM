f1d379be0e41620565d270f193ce192bb30be985
sirimainson <sirimainson@gmail.com>
2019-08-06 02:46:55 +0700

    add stats card

--- frontend/pages/Dashboard.vue
+++ frontend/pages/Dashboard.vue
@@ -64,6 +64,82 @@
           </template>
         </chart-card>
       </div>
+      <div class="md-layout-item md-medium-size-50 md-xsmall-size-100 md-size-25">
+        <stats-card data-background-color="green">
+          <template slot="header">
+            <md-icon>store</md-icon>
+          </template>
+
+          <template slot="content">
+            <p class="category">Revenue</p>
+            <h3 class="title">$34,245</h3>
+          </template>
+
+          <template slot="footer">
+            <div class="stats">
+              <md-icon>date_range</md-icon>Last 24 Hours
+            </div>
+          </template>
+        </stats-card>
+      </div>
+      <div class="md-layout-item md-medium-size-50 md-xsmall-size-100 md-size-25">
+        <stats-card data-background-color="orange">
+          <template slot="header">
+            <md-icon>content_copy</md-icon>
+          </template>
+
+          <template slot="content">
+            <p class="category">Used Space</p>
+            <h3 class="title">
+              49/50
+              <small>GB</small>
+            </h3>
+          </template>
+
+          <template slot="footer">
+            <div class="stats">
+              <md-icon class="text-danger">warning</md-icon>
+              <a href="#pablo">Get More Space...</a>
+            </div>
+          </template>
+        </stats-card>
+      </div>
+      <div class="md-layout-item md-medium-size-50 md-xsmall-size-100 md-size-25">
+        <stats-card data-background-color="red">
+          <template slot="header">
+            <md-icon>info_outline</md-icon>
+          </template>
+
+          <template slot="content">
+            <p class="category">Fixed Issues</p>
+            <h3 class="title">75</h3>
+          </template>
+
+          <template slot="footer">
+            <div class="stats">
+              <md-icon>local_offer</md-icon>Tracked from Github
+            </div>
+          </template>
+        </stats-card>
+      </div>
+      <div class="md-layout-item md-medium-size-50 md-xsmall-size-100 md-size-25">
+        <stats-card data-background-color="blue">
+          <template slot="header">
+            <i class="fab fa-twitter"></i>
+          </template>
+
+          <template slot="content">
+            <p class="category">Folowers</p>
+            <h3 class="title">+245</h3>
+          </template>
+
+          <template slot="footer">
+            <div class="stats">
+              <md-icon>update</md-icon>Just Updated
+            </div>
+          </template>
+        </stats-card>
+      </div>
     </div>
   </div>
 </template>
--- frontend/plugins/global-component.js
+++ frontend/plugins/global-component.js
@@ -1,4 +1,6 @@
 import Vue from 'vue'
 import ChartCard from '../components/Card/ChartCard'
+import StatsCard from '../components/Card/StatsCard'
 
 Vue.component('chart-card', ChartCard)
+Vue.component('stats-card', StatsCard)

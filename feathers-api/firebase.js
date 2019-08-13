f84276a43412ca6d6879dfa13cf3c8674ee08be9
niawjunior <pasupol@dome.cloud>
2019-08-13 16:29:48 +0700

    Update getMessage | Sorting by desc

--- feathers-api/firebase.js
+++ feathers-api/firebase.js
@@ -6,6 +6,6 @@ const database = firebase.initializeApp(config).firestore()
 
 export default {
   async getMessage(id) {
-    return await database.collection('Message').doc(id).collection('inbox').get()
+    return await database.collection('Message').doc(id).collection('inbox').orderBy("createdAt", "desc").get()
   }
 };

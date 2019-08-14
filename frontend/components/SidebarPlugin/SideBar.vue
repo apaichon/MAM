53dd7cb39e3c12182804b8ae7db10035db03abf5
adadesions <adadesions@gmail.com>
2019-08-14 14:40:59 +0700

    enhance logo and text on sidebar

--- frontend/components/SidebarPlugin/SideBar.vue
+++ frontend/components/SidebarPlugin/SideBar.vue
@@ -47,7 +47,7 @@ export default {
   props: {
     title: {
       type: String,
-      default: "Vue MD"
+      default: "Thai Stringer"
     },
     backgroundImage: {
       type: String,
@@ -55,7 +55,7 @@ export default {
     },
     imgLogo: {
       type: String,
-      default: require("~/assets/img/vue-logo.png")
+      default: require("~/assets/img/ts_logo.png")
     },
     activeColor: {
       type: String,
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -5,26 +5,9 @@
       <h1 class="title" style="line-height: 100px;">
         frontend
       </h1>
-      <md-button class="md-raised md-primary">Primary</md-button>
       <h2 class="subtitle" style="line-height: 50px;">
         MAM Project
       </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
     </div>
   </div>
 </template>

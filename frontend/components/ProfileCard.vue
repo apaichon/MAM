e00f351395b1fc0bc202d5fa8e10edb3ffdd38ec
nicscthongchai <nicscthongchai@gmail.com>
2019-08-17 17:09:01 +0700

    Update profile page

--- frontend/components/ProfileCard.vue
+++ frontend/components/ProfileCard.vue
@@ -5,53 +5,55 @@
         <div class="md-title">Edit Profile</div>
       </md-card-header>
 
-      <md-card-content style="padding-top: 20px;">
-        <div style="display: flex; justify-content: center; margin: 10px 0;">
-          <div
-            class="profile-image-box"
-            @mouseenter="showChangeImage"
-            @mouseleave="hideChangeImage"
-            @click="changeProfileImage"
-          >
-            <div id="profile-image">
-              <span class="change-profile-image">Change</span>
+      <form @submit="updateProfile">
+        <md-card-content style="padding-top: 20px;">
+          <div style="display: flex; justify-content: center; margin: 10px 0;">
+            <div
+              class="profile-image-box"
+              @mouseenter="showChangeImage"
+              @mouseleave="hideChangeImage"
+              @click="changeProfileImage"
+            >
+              <div id="profile-image">
+                <span class="change-profile-image">Change</span>
+              </div>
             </div>
           </div>
-        </div>
-        <div style="display: flex; text-align: center">
-          <div class="font-2x balance-panel" style="width: 50%">
-            <i class="fas fa-coins"></i> 0.00
+          <div style="display: flex; text-align: center">
+            <div class="font-2x balance-panel" style="width: 50%">
+              <i class="fas fa-coins"></i> 0.00
+            </div>
+            <div class="font-2x balance-panel" style="width: 50%">
+              <i class="fas fa-dollar-sign"></i> 0.00
+            </div>
           </div>
-          <div class="font-2x balance-panel" style="width: 50%">
-            <i class="fas fa-dollar-sign"></i> 0.00
-          </div>
-        </div>
-        <md-field>
-          <label>Email</label>
-          <md-input v-model="profile.email" disabled></md-input>
-          <span class="md-helper-text"></span>
-        </md-field>
-        <md-field>
-          <label>First Name</label>
-          <md-input v-model="profile.firstName"></md-input>
-          <span class="md-helper-text"></span>
-        </md-field>
-        <md-field>
-          <label>Last Name</label>
-          <md-input v-model="profile.lastName"></md-input>
-          <span class="md-helper-text"></span>
-        </md-field>
-        <md-field>
-          <label>Mobile No.</label>
-          <md-input v-model="profile.mobileNo"></md-input>
-          <span class="md-helper-text"></span>
-        </md-field>
-        <div class="md-layout-item md-size-100"></div>
-      </md-card-content>
 
-      <md-card-actions>
-        <md-button class="md-primary" @click="updateProfile">Update</md-button>
-      </md-card-actions>
+          <md-field>
+            <label>Email</label>
+            <md-input v-model="profile.email" disabled></md-input>
+            <span class="md-helper-text"></span>
+          </md-field>
+          <md-field>
+            <label>First Name</label>
+            <md-input v-model="profile.firstName"></md-input>
+            <span class="md-helper-text"></span>
+          </md-field>
+          <md-field>
+            <label>Last Name</label>
+            <md-input v-model="profile.lastName"></md-input>
+            <span class="md-helper-text"></span>
+          </md-field>
+          <md-field>
+            <label>Mobile No.</label>
+            <md-input v-model="profile.mobileNo"></md-input>
+            <span class="md-helper-text"></span>
+          </md-field>
+        </md-card-content>
+
+        <md-card-actions>
+          <md-button class="md-primary" type="submit" value="submit">Update</md-button>
+        </md-card-actions>
+      </form>
 
       <div ref="loader" class="card-loader">
         <div>
@@ -109,7 +111,8 @@ export default {
         this.cardLoaded();
       });
     },
-    updateProfile() {
+    updateProfile(e) {
+      e.preventDefault();
       this.cardLoading();
 
       const reqBody = {

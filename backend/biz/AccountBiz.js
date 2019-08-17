eac7b2cb6dcb34d18c60c06cbd322bf14fb95b15
nicscthongchai <nicscthongchai@gmail.com>
2019-08-17 09:36:04 +0700

    Add change password method to account biz

--- backend/biz/AccountBiz.js
+++ backend/biz/AccountBiz.js
@@ -1,10 +1,43 @@
 const { BusinessObjects } = require('../PuppyFramework');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 class AccountBiz extends BusinessObjects.BizBase {
     constructor(config) {
         config.collectionName = 'account';
         config.dbName = 'thaistringers';
         super(config);
+    }
+
+    async Add(request) {
+        const hash = await bcrypt.hash(request.data.password, saltRounds);
+        request.data.password = hash;
+
+        let result = await this.Execute('Insert', request);
+        return result;
+    }
+
+    async ChangePassword(dataWithCondition) {
+        await this.DataObject.Open();
+
+        if (dataWithCondition.data.oldPassword && dataWithCondition.data.newPassword) {
+            const account = (await this.DataObject['FindOne']({ filter: dataWithCondition.condition })).shift();
+            const match = await bcrypt.compare(dataWithCondition.data.oldPassword, account.password);
+
+            if (match) {
+                const hash = await bcrypt.hash(dataWithCondition.data.newPassword, saltRounds);
+                dataWithCondition.data['password'] = hash;
+            } else {
+                throw 'Password is not match!';
+            }
+        }
+        delete dataWithCondition.data.oldPassword;
+        delete dataWithCondition.data.newPassword;
+
+        let result = await this.DataObject['Update'](dataWithCondition);
+
+        await !this.DataObject.Close();
+        return result;
     }
 }
 

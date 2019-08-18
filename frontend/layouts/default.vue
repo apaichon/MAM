8cb38ebd1ff511a654894cee0c8062cf3f963be7
sirimainson <sirimainson@gmail.com>
2019-08-18 21:16:47 +0700

    reset sidebar

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,23 +1,24 @@
 <template>
-  <!-- <div class="wrapper" :class="{ 'nav-open': $sidebar.showSidebar }"> -->
+  <div class="wrapper" :class="{ 'nav-open': $sidebar.showSidebar }">
     <!-- <notifications></notifications> -->
 
-    <!-- <side-bar>
+    <side-bar>
       <mobile-menu slot="content"></mobile-menu>
       <sidebar-link to="/dashboard">
-        <md-icon>newspaper-variant-multiple</md-icon>
+        <i class="fas fa-newspaper"></i>
         <p>All News</p>
       </sidebar-link>
       <sidebar-link to="/dashboard">
-        <md-icon>photo_size_select_actual</md-icon>
+        <!-- <md-icon>photo_size_select_actual</md-icon> -->
+        <i class="fas fa-image"></i>
         <p>All Media</p>
       </sidebar-link>
-    </side-bar> -->
+    </side-bar>
 
-    <div>
-      <!-- <top-navbar></top-navbar> -->
+    <div class="main-panel">
+      <top-navbar></top-navbar>
       <nuxt />
-      <!-- <content-footer v-if="!$route.meta.hideFooter"></content-footer> -->
+      <content-footer v-if="!$route.meta.hideFooter"></content-footer>
     </div>
-  <!-- </div> -->
+  </div>
 </template>

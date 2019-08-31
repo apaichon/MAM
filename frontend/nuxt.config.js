f8d7d5d87b5aa869193eae24f0d813fbcf08017d
nicscthongchai <nicscthongchai@gmail.com>
2019-08-31 10:58:32 +0700

    Change backend base url to feathers-api

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -40,7 +40,7 @@ export default {
   /*
   ** Router
   */
-  
+
   /*
   ** Nuxt.js modules
   */
@@ -57,6 +57,6 @@ export default {
     }
   },
   env: {
-    baseUrl: 'http://localhost:3001/api/execute'
+    baseUrl: 'http://localhost:3032'
   }
 }

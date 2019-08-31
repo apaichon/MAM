ec5fee2e806600be100e4e28ff0dd16b6b1d4b37
nicscthongchai <nicscthongchai@gmail.com>
2019-08-31 10:11:43 +0700

    Handle error in services/index.js

--- feathers-api/services/index.js
+++ feathers-api/services/index.js
@@ -1,14 +1,21 @@
-const admin = require('firebase-admin')
-const adminConfig = require('../config/firebase-admin.json')
-admin.initializeApp({
-  credential: admin.credential.cert(adminConfig)
-})
-const firestore = admin.firestore()
-
 module.exports = {
   register(app) {
-    const path = require('path');
-    const fs = require('fs');
+    const path = require('path')
+    const fs = require('fs')
+    const admin = require('firebase-admin')
+
+    try {
+      const adminConfig = require('../config/firebase-admin.json')
+      admin.initializeApp({
+        credential: admin.credential.cert(adminConfig)
+      })
+    } catch (error) {
+      console.error('[service]', 'Fail to initialize firebase admin')
+      console.error('[service]', error.message)
+      return false
+    }
+
+    const firestore = admin.firestore()
 
     for (const filename of fs.readdirSync(__dirname)) {
       if (filename === path.basename(__filename))
